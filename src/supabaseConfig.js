0d5f0c4001292dc60988fae25d78d8ca6e562b42
marusalinass-23 <maru.salinass@gmail.com>
2026-08-01 01:26:34 -0600

    Update supabaseConfig.js

--- src/supabaseConfig.js
+++ src/supabaseConfig.js
@@ -1,5 +1,5 @@
 
 export const supabaseConfig = {
-  url: "https://prqtipjdqchwyoqkolud.supabase.co/rest/v1/",
+  url: "https://prqtipjdqchwyoqkolud.supabase.co",
   anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBycXRpcGpkcWNod3lvcWtvbHVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU1NDAyMTAsImV4cCI6MjEwMTExNjIxMH0.XxGAhJm3pQDBGA18eZ4N2X3m_KuN4IWYECdrT8PT5no",
 };
